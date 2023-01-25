720e55fa52d0a16f60fd9e7a48f03dddd4741bc9
bachikhsoukaina <bachikhsoukaina@gmail.com>
2023-01-25 10:09:52 +0100

    optimize code

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
-import { robots } from "./Data/Robot";
-import CardList from "./Components/CardList";
-import SearchBox from "./Components/SearchBox";
+import { robots } from "../Data/Robot";
+import CardList from "../Components/CardList";
+import SearchBox from "../Components/SearchBox";
+import Scroll from "../Components/Scroll";
 
 
 class App extends Component {
@@ -23,14 +24,18 @@ class App extends Component {
     this.setState({searchfield: event.target.value})  
   }
   render(){
-    const filteredRobots=this.state.robots.filter(robot=>{
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    const {robots, searchfield} = this.state;
+    const filteredRobots=robots.filter(robot=>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase())
     })
     return(
       <div className="tc">
       <h2>RobotFriends</h2>
       <SearchBox searchChange={this.onSearchChange}/>
-      <CardList robots={filteredRobots}/>
+      <Scroll>
+        <CardList robots={filteredRobots}/>
+      </Scroll>
+      
       </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import App from './App';
+import App from './Containers/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root'));
